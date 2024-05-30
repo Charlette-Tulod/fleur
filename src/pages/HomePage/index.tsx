b486aaf28fd6721ec53e22f0b867903794c0af0c
Charlette Tulod <charlettevibar@gmail.com>
2024-05-30 14:30:44 +0800

    Add home page

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,17 @@
+import Banner from '../../components/Banner';
+import ShippingInfo from '../../components/ShippingInfo';
+import SubBanner from '../../components/SubBanner';
+import FlowerLists from '../../components/FlowerLists';
+
+function HomePage() {
+  return (
+    <>
+      <Banner />
+      <ShippingInfo />
+      <SubBanner />
+      <FlowerLists />
+    </>
+  );
+}
+
+export default HomePage;
