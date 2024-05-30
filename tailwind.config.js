1b6dd9e961e4077de25744aad31f77ece482b16c
Charlette Tulod <charlettevibar@gmail.com>
2024-05-30 14:25:36 +0800

    Add packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '30/70': '28% 70%',
+      },
+    },
+    colors: {
+      rose: '#ffe4e6',
+      darkerrose: '#fda4af',
+      brown: '#754328',
+      lightrose: '#fff1f2',
+      cwhite: '#ffffff',
+      red: '#ef4444',
+    },
   },
   plugins: [],
 };
