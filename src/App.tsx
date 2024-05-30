a682c6ce89ace1b0b4349522be8fa5f657e08e13
Charlette Tulod <charlettevibar@gmail.com>
2024-05-30 23:54:08 +0800

    Add routing of recipedetails

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import AllFlowers from './pages/AllFlowers';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import FlowerDetailsPage from './pages/FlowerDetailsPage';
 import NavLayout from './layouts/NavLayout';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<NavLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/flowers" element={<AllFlowers />} />
+        <Route path="/flowers/:id" element={<FlowerDetailsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Route>
