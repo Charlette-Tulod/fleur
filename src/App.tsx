9015ec530d0dfd16b1b518e7d534f9dc06d4e2e9
Charlette Tulod <charlettevibar@gmail.com>
2024-05-31 23:19:33 +0800

    Add private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AllFlowers from './pages/AllFlowers';
@@ -9,14 +10,33 @@ import AboutUsPage from './pages/AboutUsPage';
 import ContactUsPage from './pages/ContactUsPage';
 import FAQPage from './pages/FAQPage';
 import NotFoundPage from './pages/NotFoundPage';
+import useAuth from './hooks/userHooks/useUserAuth';
+import useCartStore from './store/useCartStore';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const { user } = useAuth();
+  const fetchCart = useCartStore((state) => state.fetchCart);
+
+  useEffect(() => {
+    if (user) {
+      fetchCart(user.id);
+    }
+  }, [user, fetchCart]);
+
   return (
     <Routes>
       <Route path="/" element={<NavLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/flowers" element={<AllFlowers />} />
-        <Route path="/flowers/:id" element={<FlowerDetailsPage />} />
+        <Route
+          path="/flowers/:id"
+          element={
+            <PrivateRoute>
+              <FlowerDetailsPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/FAQ" element={<ContactUsPage />} />
         <Route path="/contact" element={<FAQPage />} />
