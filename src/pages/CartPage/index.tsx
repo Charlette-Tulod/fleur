311cd226c4cfc9ac999fe6923d491c14d77a3586
Charlette Tulod <charlettevibar@gmail.com>
2024-06-01 01:13:03 +0800

    Add cart page implemetation

---
+++ src/pages/CartPage/index.tsx
@@ -0,0 +1,237 @@
+import { useState, useEffect } from 'react';
+import {
+  Container,
+  Typography,
+  Button,
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  TextField,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useNavigate } from 'react-router-dom';
+import useCartStore from '../../store/useCartStore';
+
+function CartPage() {
+  const { cart, clearCart, removeFromCart, updateCartItemQuantity } =
+    useCartStore();
+  const navigate = useNavigate();
+  const [editMode, setEditMode] = useState<{ [key: string]: boolean }>({});
+  const [quantities, setQuantities] = useState<{ [key: string]: number }>({});
+  const [total, setTotal] = useState<number>(0);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      const totalAmount = cart.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      setTotal(totalAmount);
+    };
+
+    calculateTotal();
+  }, [cart]);
+
+  const handleCheckout = () => {
+    clearCart();
+    navigate('/ordersuccess');
+  };
+
+  const handleDelete = (id: string) => {
+    removeFromCart(id);
+  };
+
+  const handleEdit = (id: string) => {
+    setEditMode((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
+  const handleQuantityChange = (id: string, quantity: number) => {
+    setQuantities((prev) => ({ ...prev, [id]: quantity }));
+  };
+
+  const handleSave = (id: string) => {
+    updateCartItemQuantity(id, quantities[id]);
+    setEditMode((prev) => ({ ...prev, [id]: false }));
+  };
+
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        backgroundColor: '#fff1f2',
+        boxShadow: '0px 4px 8px rgba(0, 0.1, 0.1, 0.1)',
+        borderRadius: 2,
+        mt: 5,
+        mb: 5,
+      }}
+    >
+      <Box
+        sx={{
+          p: 5,
+        }}
+      >
+        <Typography variant="h4" fontWeight="bold" color="customColors.brown">
+          Your Cart
+        </Typography>
+
+        {cart.length === 0 ? (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              mt: 5,
+            }}
+          >
+            <ShoppingCartIcon sx={{ fontSize: 120, color: '#754328', mb: 5 }} />
+            <Typography variant="h4" color="customColors.brown">
+              Your cart is empty.
+            </Typography>
+          </Box>
+        ) : (
+          <>
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              sx={{
+                mt: 3,
+                mb: 5,
+              }}
+            >
+              <Button
+                variant="contained"
+                onClick={clearCart}
+                sx={{
+                  backgroundColor: '#ffffff',
+                }}
+              >
+                Clear Cart
+              </Button>
+            </Box>
+            <Grid container spacing={2}>
+              {cart.map((item) => (
+                <Grid item xs={12} key={item.id}>
+                  <Card
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      width: '100%',
+                      backgroundColor: '#ffffff',
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{ width: 120, height: 150 }}
+                      image={item.image}
+                      alt={item.title}
+                    />
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        flex: 2,
+                      }}
+                    >
+                      <CardContent>
+                        <Typography
+                          variant="h6"
+                          gutterBottom
+                          color="customColors.brown"
+                        >
+                          {item.title}
+                        </Typography>
+                        {editMode[item.id] ? (
+                          <TextField
+                            type="number"
+                            value={quantities[item.id] ?? item.quantity}
+                            onChange={(e) =>
+                              handleQuantityChange(
+                                item.id,
+                                parseInt(e.target.value, 10)
+                              )
+                            }
+                            inputProps={{ min: 1 }}
+                            sx={{
+                              width: '60%',
+                              minWidth: 30,
+                              height: '20%',
+                              minHeight: 30,
+                              whiteSpace: 'nowrap',
+                            }}
+                          />
+                        ) : (
+                          <Typography variant="body2">
+                            Quantity: {item.quantity}
+                          </Typography>
+                        )}
+                        <Typography variant="body2">
+                          Price: ₱{item.price}
+                        </Typography>
+                        <Typography variant="body2">
+                          Subtotal: ₱{(item.price * item.quantity).toFixed(2)}
+                        </Typography>
+                      </CardContent>
+                    </Box>
+                    <Box>
+                      <CardContent>
+                        <IconButton onClick={() => handleDelete(item.id)}>
+                          <DeleteIcon />
+                        </IconButton>
+                        {editMode[item.id] ? (
+                          <IconButton onClick={() => handleSave(item.id)}>
+                            <SaveIcon />
+                          </IconButton>
+                        ) : (
+                          <IconButton onClick={() => handleEdit(item.id)}>
+                            <EditIcon />
+                          </IconButton>
+                        )}
+                      </CardContent>
+                    </Box>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+
+            <Box
+              mt={6}
+              display="flex"
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="#754328"
+                sx={{
+                  textAlign: 'right',
+                  ml: 2,
+                }}
+              >
+                Total: ₱{total.toFixed(2)}
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={handleCheckout}
+                sx={{
+                  backgroundColor: '#ffffff',
+                  ml: 2,
+                }}
+              >
+                Checkout
+              </Button>
+            </Box>
+          </>
+        )}
+      </Box>
+    </Container>
+  );
+}
+
+export default CartPage;
